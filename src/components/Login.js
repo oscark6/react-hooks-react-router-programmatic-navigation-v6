f161b1ef794adcc4fb8c97463e708bd8b36fe241
oscark6 <oscar.kimani1@student.moringaschool.com>
2024-05-26 20:55:01 +0300

    done

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,9 @@
 import { useState } from "react";
+import { useOutletContext } from "react-router-dom";
 
 function Login() {
+  const login = useOutletContext();
+
   const [formData, setFormData] = useState({
     username: "",
     password: "",
@@ -15,6 +18,8 @@ function Login() {
 
   function handleLogin(e) {
     e.preventDefault();
+    login();
+    
   }
 
   return (
